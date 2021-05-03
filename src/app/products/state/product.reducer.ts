08da64145beb24217b103b4acd3670670f9abf14
unknown <abderrahmane.benchekroun@gmail.com>
2021-05-03 10:03:05 +0200

    version 2.0

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,14 +1,67 @@
 
-import { createReducer, on, createAction } from '@ngrx/store';
+import { createReducer, on, createAction, createFeatureSelector, createSelector } from '@ngrx/store';
+import { Product } from '../product';
+import * as ProductActions from './product.actions';
+
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector (
+  getProductFeatureState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector (
+  getProductFeatureState,
+  state => state.products
+);
 
 export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] Toggle Product Code'), state => {
+  { showProductCode: true } as ProductState,
+  on(ProductActions.toggleProduct, (state): ProductState => {
     console.log('original state : ' + JSON.stringify(state));
     return {
       ...state,
       showProductCode: !state.showProductCode
     };
-  })
+  }),
+
+  on(ProductActions.setCurrentProduct, (state, action): ProductState => {
+    return {
+      ...state,
+      currentProduct: action.product
+    }
+  }),
+
+  on(ProductActions.clearCurrentProduct, (state): ProductState => {
+    return {
+      ...state,
+      currentProduct: null
+    }
+  }),
+  
+  on(ProductActions.initializeCurrentProduct, (state): ProductState => {
+    return {
+      ...state,
+      currentProduct: {
+        id: 0,
+        productName: '',
+        productCode: 'New',
+        description: '',
+        starRating: 0
+      }
+    }
+  }),
 );
 
